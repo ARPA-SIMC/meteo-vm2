8fe24d31ea1aa1a3c04acbbbb44d3bd9a776197b
Emanuele Di Giacomo <edigiacomo@arpae.it>
2018-10-19 14:07:01 +0200

    Messages

--- src/meteo-vm2-to-bufr.cc
+++ src/meteo-vm2-to-bufr.cc
@@ -188,8 +188,8 @@ int main(int argc, const char** argv)
       int top = lua_gettop(L);
 
       try {
-        dballe::Messages msgs;
-        std::uniqe_ptr<dballe::Message> msg = dballe::Message::create(dballe::MessageType::GENERIC);
+        std::vector<std::shared_ptr<Message> msgs;
+        std::unique_ptr<dballe::Message> msg = dballe::Message::create(dballe::MessageType::GENERIC);
         // date
         msg.set("year", value.year);
         msg.set("year", value.month);

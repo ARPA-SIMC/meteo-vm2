9a4315656d0bd5777ec0ad181100677c949fb0b6
Emanuele Di Giacomo <edigiacomo@arpae.it>
2018-11-26 09:31:21 +0100

    Typo

--- .travis-build.sh
+++ .travis-build.sh
@@ -35,7 +35,7 @@ then
     mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
     cp fedora/SPECS/meteo-vm2.spec ~/rpmbuild/SPECS/meteo-vm2.spec
     git archive --prefix=$pkgname/ --format=tar HEAD | gzip -c > ~/rpmbuild/SOURCES/$pkgname.tar.gz
-    rpmbuild -ba --define "srcarchivename $pkgname" ~/rpmbuild/SPECS/meteo-vm.spec
+    rpmbuild -ba --define "srcarchivename $pkgname" ~/rpmbuild/SPECS/meteo-vm2.spec
 else
     autoreconf -ifv
     ./configure

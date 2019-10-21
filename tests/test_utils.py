adfa6e864d0f6467f940bf2397eb986f2f22c355
Emanuele Di Giacomo <edigiacomo@arpae.it>
2019-10-21 11:16:40 +0200

    More tests for meteovm2_to_bufr

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,19 @@ import dballe
 
 class TestUtils(TestCase):
     def test_meteovm2_to_bufr(self):
-        inputstring = StringIO("201901020300,1,158,3.5,,,")
+        inputstring = StringIO((
+            # Valid
+            "201901020300,1,158,3.5,,,\n"
+            "201901020300,1,158,3.5,,,000000000\n"
+            # Manual invalidation
+            "201901020300,1,158,3.5,,,100000000\n"
+            # Gross error
+            "201901020300,1,158,3.5,,,054000000\n"
+            # Manual invalidation + gross error
+            "201901020300,1,158,3.5,,,154000000\n"
+            # Manual replacement
+            "201901020300,1,158,3.5,3.6,,200000000\n"
+        ))
         outputstring = BytesIO()
 
         utils.meteovm2_to_bufr(inputstring, outputstring, "tests/table.json")
@@ -18,18 +30,23 @@ class TestUtils(TestCase):
         importer = dballe.Importer("BUFR")
 
         data = []
+        attrs = []
 
         with importer.from_file(outputstring) as f:
             for msgs in f:
                 for msg in msgs:
                     for d in msg.query_data():
                         data.append(d.data_dict)
+                        attrs.append(d["variable"].get_attrs())
 
-        self.assertEqual(len(data), 1)
+        self.assertEqual(len(data), 2)
+
         self.assertEqual(data[0].get("ident"), None)
         self.assertEqual(data[0]["B12101"], 3.5 + 273.15)
         self.assertEqual(data[0]["level"], dballe.Level(103, 2000))
         self.assertEqual(data[0]["trange"], dballe.Trange(254, 0, 0))
+
+        self.assertEqual(attrs[1], [dballe.var("B33196", 1)])
 
     def test_meteovm2_to_bufr_invalid(self):
         for line in (
